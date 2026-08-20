b9879dd8b1ad9055ae9e7c458183551dd0206711
Anastasiia Pokasova <pokasovaai@gmail.com>
2026-08-20 04:59:12 +0300

    fix message sending methods

--- scheduler.py
+++ scheduler.py
@@ -1,9 +1,17 @@
+import logging
 import random
 from asyncio import sleep
 from datetime import datetime
 
 from telethon import TelegramClient, events
 from telethon.tl.custom import Message
+
+
+logging.basicConfig(level=logging.INFO,
+                    format="[%(asctime)s: %(levelname)-8s %(name)-s] %(message)s",
+                    handlers=[logging.FileHandler("autoposting.log"), logging.StreamHandler()])
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 class TGScheduler:
@@ -47,7 +55,6 @@ class TGScheduler:
         time_now = f"{now.hour:02}:{now.minute:02}"
         return time_now
 
-
     async def sample_messages(self, posts_num: int = 1) -> list | None:
         messages = await self.client.get_messages(self.source_channel, limit=None)
         messages_ids = [m.id for m in messages]
@@ -60,26 +67,30 @@ class TGScheduler:
         messages = await self.client.get_messages(self.source_channel, ids=random_ids)
         return messages
 
-    def create_schedule(self, posts_num: int = 5):
+    def create_schedule(self, posts_num: int = 3):
         self.schedule = []
 
         for i in range(posts_num):
-            hours = random.randint(9, 23)
+            hours = random.randint(10, 23)
             mins  = random.randint(0, 59)
             self.schedule.append((f"{hours:02}:{mins:02}", self.source_channel, self.target_channel))
 
         self.schedule = sorted(self.schedule, key=lambda x: self.convert_to_mins(x[0]))
 
     async def send_message(self, event) -> None:
-        await self.client.forward_messages(
-            self.target_channel,
-            event.messages
-        )
-
-    async def send_album(self, event) -> None:
+        logging.info(f"Sending message {event}")
+        msg = event.message
+        msg.raw_text = ""
         await self.client.forward_messages(
             self.target_channel,
             event.message
+        )
+
+    async def send_album(self, event) -> None:
+        logging.info(f"Sending album {event}")
+        await self.client.forward_messages(
+            self.target_channel,
+            event.messages
         )
 
     async def forward_message(self, message: Message) -> None:
@@ -94,7 +105,8 @@ class TGScheduler:
     async def post(self):
         source_channel_id: int = await self.get_chat_id(self.source_channel)
         target_channel_id: int = await self.get_chat_id(self.target_channel)
-
+        logger.info(f"{self.source_channel} -> {source_channel_id}")
+        logger.info(f"{self.target_channel} -> {target_channel_id}")
         if source_channel_id is None or target_channel_id is None:
             return
 
@@ -104,7 +116,8 @@ class TGScheduler:
 
         for message in sample_messages:
             try:
-                await self.send_message(message)
+                logger.info(f"Trying to post {message.id} from {self.source_channel} to {self.target_channel}")
+                await self.forward_message(message)
             except Exception:
                 return
             else:
@@ -115,6 +128,9 @@ class TGScheduler:
         while True:
             if time_now == "00:00" and len(self.schedule) == 0:
                 self.create_schedule()
+                for event in self.schedule:
+                    e_time, e_queue, e_target = event
+                    logger.info(f"[{e_time}] {e_queue} -> {e_target}")
 
             while len(self.schedule) > 0 and (self.convert_to_mins(time_now) > self.convert_to_mins(self.schedule[0][0])):
                 self.schedule.pop(0)
